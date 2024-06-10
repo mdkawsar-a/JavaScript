ab3cb146e3a8cac257ff3159cd4f6096cf60227e
Kh mdkawsar <85445753+mdkawsar-a@users.noreply.github.com>
2024-06-10 12:25:48 +0000

    String

---
+++ basics_js/4_string.js
@@ -0,0 +1,28 @@
+ const name = "kawsar"
+ const age = 24
+
+//  console.log("Hello " + name + " Your age is " + age);
+
+// console.log(`Hello ${name} Your age is ${age}`);
+
+let game = new String("kawsar") 
+
+// console.log(game.indexOf('w'));
+
+const newString = game.substring( 3, 6 )
+ 
+// console.log(newString);
+ 
+const gameName = " Free Fire game"
+const gameNameToSlice = gameName.slice(-8, 4)
+// console.log(gameNameToSlice);
+
+// console.log(gameName.trim());
+
+const url = "https://kawsar.com/kawsar%20choudhary"
+
+// console.log(url.replace('%20', '-'));
+
+// console.log(url.includes('sumi'));
+
+console.log(gameName.split('-'));
