7178985a0e86c62fbd7ca3be9629c7b2fa91a4a5
Kh mdkawsar <85445753+mdkawsar-a@users.noreply.github.com>
2024-06-10 04:42:02 +0000

    Convertion

---
+++ basics_js/2_convertional.js
@@ -0,0 +1,27 @@
+let score = "false"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score) 
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33 
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "kawsar"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "kawsar" => true
+
+let age = 25
+
+let stringAge = String(age)
+console.log(stringAge);
+console.log(typeof stringAge);
