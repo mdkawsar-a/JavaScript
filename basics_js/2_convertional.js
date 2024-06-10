9440236cf3b5e182c2bce475d919f4e9d0fcb393
Kh mdkawsar <85445753+mdkawsar-a@users.noreply.github.com>
2024-06-10 05:16:45 +0000

    Convertion

--- basics_js/2_convertional.js
+++ basics_js/2_convertional.js
@@ -23,5 +23,46 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let age = 25
 
 let stringAge = String(age)
-console.log(stringAge);
-console.log(typeof stringAge);
+// console.log(stringAge);
+// console.log(typeof stringAge);
+
+
+// ********************** Operations *************************
+
+let value = 3 
+ let negValue = -value
+//  console.log(negValue);
+
+// console.log(2+2);
+// console.log(3-2);
+// console.log(3*5);
+// console.log(5**3);
+// console.log(5/2);
+// console.log(2%5);
+
+
+let str1 = "Hello"
+let str2 = " kawsar"
+let str3 = str1 + str2
+
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 3 * 3 
+
+
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
