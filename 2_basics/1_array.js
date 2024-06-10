6118a2714a1d1d93a04ef8cee5162011d2a9a6d0
Kh mdkawsar <85445753+mdkawsar-a@users.noreply.github.com>
2024-06-10 17:16:22 +0000

    Array Methods

---
+++ 2_basics/1_array.js
@@ -0,0 +1,20 @@
+// Array 
+
+const myArray = [0, 1, 2, 3, 4, 5]
+const myFriend = ["Sohel", "Sumi", "Jonaid"]
+
+const myArray2 = new Array(1, 2, 3, 4, 5)
+// console.log(myArray2[0]);
+
+// Array Methods
+
+// myArray.push(6)
+// myArray.push(7)
+// myArray.pop()
+
+// myArray.unshift(9)
+// myArray.shift()
+
+
+
+console.log(myArray);
