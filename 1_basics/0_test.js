7092ed6e151f0b5f3eb5fb99936033dff16d8b72
Kh mdkawsar <85445753+mdkawsar-a@users.noreply.github.com>
2024-06-11 16:03:12 +0600

    Update 0_test.js

--- 1_basics/0_test.js
+++ 1_basics/0_test.js
@@ -1,1 +1,18 @@
 
+const number = 14355
+
+let emaillId = "kawsar@gmail.com"
+
+console.table([number, emaillId]);
+
+// number => 2 
+// string => "" 
+// null => 
+// undefined
+// boolean => true / false
+// symbol => unique
+
+//object =>  
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
