f99c828ca0cd26cfecff0c4e6e75f0ca528cc64a
Kh mdkawsar <85445753+mdkawsar-a@users.noreply.github.com>
2024-06-12 17:34:34 +0000

    scope

---
+++ 2_basics/4_scopes.js
@@ -0,0 +1,51 @@
+
+// var z = 300
+let x = 300
+if(true){
+    let x = 10
+    const y = 20
+    // console.log(x);
+}
+
+// console.log(x);
+// console.log(y);
+
+
+function one(){
+    const username = "kawsar"
+
+    function two(){
+        const website = "google.com"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+// one()
+
+if(true){
+    const username = "kawsar"
+
+    if(username === "kawsar"){
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(usernaem);
+
+
+//+++++++++++++++++++++++++++++++++++
+
+console.log(addOne(4));
+
+function addOne(num){
+    return num + 2
+}
+
+const addTwo = function(num){
+    return num + 5
+    
+}
+addTwo(5)
