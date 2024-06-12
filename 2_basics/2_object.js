bd83b2fa5b05e7fbbf279382103d083f035890f0
Kh mdkawsar <85445753+mdkawsar-a@users.noreply.github.com>
2024-06-12 08:00:20 +0000

    object

--- 2_basics/2_object.js
+++ 2_basics/2_object.js
@@ -35,5 +35,81 @@ jsUser.greetingTwo = function(){
     console.log("Hello JS User");
 }
 
-console.log(jsUser.greeting());
-console.log(jsUser.greetingTwo());
+// console.log(jsUser.greeting());
+// console.log(jsUser.greetingTwo());
+
+
+// const tinderUser = new Object() // singleton object
+
+const tinderUser = {}        // Non singleton object
+
+tinderUser.name = "Kawsar"
+tinderUser.id = "123abc"
+tinderUser.email = "Kawsar@google.com"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser["email"]);
+
+const regulerUser = {
+    email: "kawsar@microsoft.com",
+    fullname: {
+        userfullname: {
+            firstname: "kawsar",
+            lastname: "Ahmad"
+        }
+    }
+}
+
+// console.log(regulerUser.fullname.userfullname.lastname);
+
+const obj1 = {1: "a", 2: "b",}
+const obj2 = {3: "c", 4: "d",}
+
+// const obj3 = { obj1, obj2 }
+// const obj3 = Object.assign({}, obj1, obj2)
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+
+const user = [
+    {
+        id: 1,
+        email: "K@gmail.com"
+    },
+    {
+        id: 1,
+        email: "K@gmail.com"
+    },
+    {
+        id: 1,
+        email: "K@gmail.com"
+    },
+    {
+        id: 1,
+        email: "K@gmail.com"
+    },
+]
+
+// console.log(user[2].email);
+
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty("fristname"));
+
+
+const course = {
+    coursename: "JavaScript",
+    price: "9000",
+    courseInstructor: "Kawsar"
+}
+
+// console.log(course.courseInstructor);
+
+const {courseInstructor: instructor} = course
+
+// console.log(instructor);
