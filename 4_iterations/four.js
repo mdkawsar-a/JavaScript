0bc92d4a7128417750eb109daaf2682127e298b1
Kh mdkawsar <85445753+mdkawsar-a@users.noreply.github.com>
2024-06-29 15:31:09 +0000

    loop

--- 4_iterations/four.js
+++ 4_iterations/four.js
@@ -13,5 +13,14 @@ for (const key in myObject) {
 const programming = ["R", "GO", "JavaScript", "Python", "Java"]
 
 for (const key in programming) {
-    console.log(programming[key]);
+    // console.log(programming[key]);
 }
+
+// const map = new Map()
+// map.set('BD', "Bangladesh")
+// map.set('FK', "Fakistan")
+// map.set('AF', "Afghanistan")
+
+// for (const key in map) {  // Not Iterable
+//     console.log(key);
+// }
