8ae578351596e865730a9568937d0876c5da94d2
Kh mdkawsar <85445753+mdkawsar-a@users.noreply.github.com>
2024-06-29 14:16:58 +0000

    Array loop

---
+++ 4_iterations/three.js
@@ -0,0 +1,30 @@
+// Array loop
+
+// for of 
+
+// ["", "", "", ""]
+// [{}, {}, {}]
+
+const myArry = [1, 2, 3, 4, 5, 6]
+
+for (const num of myArry) {
+    // console.log(num);
+}
+
+const greetings = "!Hello World"
+for (const greet of greetings) {
+    // console.log(`Each char is ${greet}`);
+}
+
+// Maps
+
+const map = new Map()
+map.set('BD', "Bangladesh")
+map.set('FK', "Fakistan")
+map.set('AF', "Afghanistan")
+
+// console.log(map.get('BD'));
+
+for (const [i, j] of map) {
+    // console.log(i, "= ", j);
+}
