295796bb2d34e2e9ff1f1938de809ebd7e2e3f9a
Kh mdkawsar <85445753+mdkawsar-a@users.noreply.github.com>
2024-06-29 13:32:45 +0000

    do loop

--- 4_iterations/two.js
+++ 4_iterations/two.js
@@ -1,11 +1,20 @@
 
 let x = 0
 while (x <= 10) {
-    console.log(`Value of x is ${x}`);
+    // console.log(`Value of x is ${x}`);
     x = x + 2
 }
 
 let myArray = ["Red", "Black"]
-while (condition) {
-    
+let y = 0
+while ( y < myArray.length ) {
+    // console.log(`${myArray[y]}`);
+    y = y + 1
 }
+
+let score = 11
+
+do {
+    console.log(`Score is : ${score}`);
+    score++
+} while (score <= 10);
