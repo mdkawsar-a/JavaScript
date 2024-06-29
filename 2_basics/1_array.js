df32689854b328b45bbdb377fb8300514dca3224
Kh mdkawsar <85445753+mdkawsar-a@users.noreply.github.com>
2024-06-29 12:01:28 +0000

    Array

--- 2_basics/1_array.js
+++ 2_basics/1_array.js
@@ -67,4 +67,5 @@ let score1 = 100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1, score2, score3));
+const scoreArray = Array.of(score1, score2, score3);
+console.log(scoreArray);
