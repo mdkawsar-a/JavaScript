5a2e6d653681bd5b4a9487cf57807ed216690d74
Kh mdkawsar <85445753+mdkawsar-a@users.noreply.github.com>
2024-06-29 13:01:58 +0000

    Terniary

--- 3_control_flow/truthy.js
+++ 3_control_flow/truthy.js
@@ -19,5 +19,23 @@ if (userEmail) {
 const emtyObj = {}
 
 if (Object.keys(emtyObj).length === 0) {
-    console.log("Object is emty");
+    // console.log("Object is emty");
 }
+
+// Nullish Coalescing Operator (??) null undefined
+
+let val1;
+// val1 = 4 ?? 30
+// val1 = null ?? 20
+// val1 = undefined ?? 10
+val1 = null ?? 15 ?? 25
+
+// console.log(val1);
+
+
+// Terniary Operator 
+// condition ? true : false
+
+const bookPrice = 200
+
+bookPrice >= 100 ? console.log("More than 100") : console.log("Less than 100"); 
