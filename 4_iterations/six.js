40d02fadc17c47dba830f7be4b61dce384acf857
Kh mdkawsar <85445753+mdkawsar-a@users.noreply.github.com>
2024-06-30 05:46:17 +0000

    filter

--- 4_iterations/six.js
+++ 4_iterations/six.js
@@ -5,7 +5,7 @@ const languageName = ["java", "javascript", "python", "ruby"]
 //     return item
 // } )
 
-// console.log(values);
+// console.log(values); // undefined
 
 const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
@@ -23,3 +23,22 @@ const numReturn = myNumbers.filter( (num) =>  {
 //     }
 // } )
 // console.log(newNumbers);
+
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+let userBooks = books.filter( (item) => (item.genre === 'History') )
+userBooks = books.filter( (bk) => {
+    return bk.publish >= 1999 && bk.genre === "Science"
+} )
+console.log(userBooks);
