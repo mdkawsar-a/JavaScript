01aa513e11f7e5b3c0f28f356669dc5d379e26ff
Kh mdkawsar <85445753+mdkawsar-a@users.noreply.github.com>
2024-07-01 13:47:21 +0000

    setting

---
+++ 4_iterations/seven.js
@@ -0,0 +1,8 @@
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const addNums = myNums.forEach( (num) => {
+    //  if (condition) {
+    //     return num + 20
+    //  }
+     console.log(num);
+} )
