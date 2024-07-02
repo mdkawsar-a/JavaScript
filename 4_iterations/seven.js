0dcb472a59f485ee41f09776af3273f2ab1f6fce
Kh mdkawsar <85445753+mdkawsar-a@users.noreply.github.com>
2024-07-02 04:34:42 +0000

    filter

--- 4_iterations/seven.js
+++ 4_iterations/seven.js
@@ -1,8 +1,10 @@
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-const addNums = myNums.forEach( (num) => {
-    //  if (condition) {
-    //     return num + 20
-    //  }
-     console.log(num);
-} )
+// const addNums = myNums.map( (num) => num + 20 )
+
+const addNums = myNums
+                .map( (num) => num * 10 )
+                .map( (num) => num + 1 )
+                .filter( (num) => num <= 61 )
+
+console.log(addNums);
